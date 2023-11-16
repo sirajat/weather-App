b6b7bd1a327a411b107ddb34fd4a87ea2cf246f7
Soofiya Binth Shajahan B <soofiyashajahan@gmail.com>
2023-11-17 00:39:44 +0530

    Updated Current time and date in App

--- src/Banner/banner.jsx
+++ src/Banner/banner.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../Banner/banner.css";
 
 function Banner() {
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
+
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    timeZoneName: "short",
+  };
+
+  const formattedDateTime = dateTime.toLocaleDateString("en-US", options);
+
   return (
     <div className="banner-image">
-      <div className="top"></div>
+      <div className="top">{formattedDateTime}</div>
     </div>
   );
 }
