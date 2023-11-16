5df1ac32634e78f1f759d57710308c346707858a
Soofiya Binth Shajahan B <soofiyashajahan@gmail.com>
2023-11-16 19:04:13 +0530

    Created the Banner and top and made some simple stylings

---
+++ src/Banner/banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Banner/banner.css";
+
+function Banner() {
+  return (
+    <div className="banner-image">
+      <div className="top"></div>
+    </div>
+  );
+}
+
+export default Banner;
